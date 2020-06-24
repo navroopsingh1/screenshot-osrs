00b7620871c11108d7ac3ec1c6877eb54595872a
paperwarfare3@gmail.com <paperwarfare3@gmail.com>
2020-06-24 16:46:59 -0400

    collected 130 images of iron rocks

--- screenshot.py
+++ screenshot.py
@@ -1,9 +1,12 @@
 import pyscreenshot as ImageGrab
 from datetime import datetime
+import time
 
 # part of the screen
-im = ImageGrab.grab(bbox=(0, 25, 765, 480))  # X1,Y1,X2,Y2
-dt = datetime.now()
-# save image file
-fname = "img/pic_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000)
-im.save(fname,'png')
+for i in range(40):
+	im = ImageGrab.grab(bbox=(0, 25, 765, 480))  # X1,Y1,X2,Y2
+	dt = datetime.now()
+	# save image file
+	fname = "img/pic_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000)
+	im.save(fname,'png')
+	time.sleep(2)
