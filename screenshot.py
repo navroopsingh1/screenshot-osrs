7df2c3ce127d02f759a05e86207d293b50e562a9
paperwarfare3@gmail.com <paperwarfare3@gmail.com>
2020-06-24 16:28:25 -0400

    added the ability to save more than one picture and adjusted screen shot size to the size of the runescape window

--- screenshot.py
+++ screenshot.py
@@ -1,7 +1,9 @@
 import pyscreenshot as ImageGrab
+from datetime import datetime
 
 # part of the screen
-im = ImageGrab.grab(bbox=(10, 10, 510, 510))  # X1,Y1,X2,Y2
-
+im = ImageGrab.grab(bbox=(0, 25, 765, 480))  # X1,Y1,X2,Y2
+dt = datetime.now()
 # save image file
-im.save('img/box.png')
+fname = "img/pic_{}.{}.png".format(dt.strftime("%H%M_%S"), dt.microsecond // 100000)
+im.save(fname,'png')
